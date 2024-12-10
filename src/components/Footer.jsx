7e6e4eaffe88a6991eb7115e116bb8bafdd2cede
Demo-1 <hamedhasanalvi8@gmail.com>
2024-12-10 19:06:03 +0600

    nfjgn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
       <div className="flex max-sm:flex-col justify-center items-center space-y-4 py-5">
         <div className="space-y-4">
           <Image className="w-[200px] h-[60px] mx-auto" src={mzLogo} alt="MZLOGO" placeholder='blur' />
-          <p className="flex text-center gap-2 text-[#9c9c9c] lg:text-xl">© 2024 mzmedia | all rights reseved by DON YEAMIN</p>
+          <p className="flex text-center gap-2 text-[#9c9c9c] lg:text-xl">© 2024 mzmedia | all rights reseved by Bolod YEAMIN</p>
         </div>
         <div className="w-12 h-12 bg-[#e7b8fa] hover:bg-[#d9aaf7] rounded-full flex justify-center items-center cursor-pointer absolute max-sm:relative max-sm:mx-auto  sm:right-6 lg:right-9 " onClick={handleScrollToTop}>
           <i class="ri-arrow-up-double-line text-slate-700 text-4xl "></i>
